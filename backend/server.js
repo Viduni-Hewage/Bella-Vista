1d2a389a0cdee06fdbfd10388b15e98e2d53338d
Viduni-Hewage <viduhewage02@gmail.com>
2025-08-09 00:22:47 +0530

    Initialize the proect and setup backend with Express and MongoDB

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { connectDB } = require('./config/db'); 
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({message: 'Something went wrong!'});
+});
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+});
