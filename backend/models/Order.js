cc2c88e96d3511aa85ce868cd21ab2dc069f25b7
Viduni-Hewage <viduhewage02@gmail.com>
2025-08-09 09:50:19 +0530

    Create order model schema

---
+++ backend/models/Order.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  products: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+      quantity: { type: Number, required: true }
+    }
+  ],
+  totalAmount: { type: Number, required: true },
+  paymentMethod: { type: String, enum: ['COD', 'Online'], required: true },
+  paymentStatus: { type: String, enum: ['Pending', 'Paid'], default: 'Pending' },
+  status: { type: String, default: 'Pending' } 
+}, { timestamps: true });
+
+module.exports = mongoose.model('Order', orderSchema);
