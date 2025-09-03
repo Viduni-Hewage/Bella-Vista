70a1caee7af18b0eaee06c08c447ae698d33cc7a
Viduni-Hewage <viduhewage02@gmail.com>
2025-09-03 12:20:30 +0530

    Modify layout page

--- frontend/src/hoc/Layout.jsx
+++ frontend/src/hoc/Layout.jsx
@@ -1,12 +1,25 @@
 import { Outlet } from 'react-router-dom';
 import WebHeader from "../component/header";
 import WebFooter from '../component/footer';
+import { useState, useEffect } from 'react';
 
 const Layout = () => {
+  const [showHeader, setShowHeader] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setShowHeader(scrollY > 200);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div>
-      <WebHeader visible={true} /> 
-      <main style={{ marginTop: "10vh" }}> 
+      <WebHeader visible={showHeader} />
+      <main> 
         <Outlet /> 
       </main>
       <WebFooter />
