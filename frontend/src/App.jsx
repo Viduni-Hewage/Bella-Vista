d4a12356f872a624db234830848ea7b9d1ceafe4
Viduni-Hewage <viduhewage02@gmail.com>
2025-09-03 15:20:14 +0530

    Fix scroll to top on route change for SPA navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,23 +5,29 @@ import ShopByCategory from './pages/shopByCategory';
 import Login from './pages/login';
 import SignUp from './pages/signUp';
 import WorldOfBellaVista from './pages/worldOfBellaVista';
+import News from './pages/news';
+import ScrollToTop from './component/ScrollToTop';
 
 const App = () => {
   return (
-    <Routes>      
+
+    <>
+      <ScrollToTop />
+      <Routes>      
         
-      <Route index element={<Home />} />  
-      <Route path="/login" element={<Login />} />
-      <Route path="/sign-up" element={<SignUp />} />
-
+        <Route index element={<Home />} />  
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
      
-      <Route path="/" element={<Layout />}>        
-        <Route path="shop-by category" element={<ShopByCategory />} />
-        <Route path="world" element={<WorldOfBellaVista />} />
-       
-      </Route>
-
-    </Routes>
+        <Route path="/" element={<Layout />}>        
+          <Route path="shop-by category" element={<ShopByCategory />} />
+          <Route path="world" element={<WorldOfBellaVista />} />
+          <Route path="news" element={<News />} />
+        
+        </Route>
+      </Routes>
+    </>
+    
   );
 };
 
